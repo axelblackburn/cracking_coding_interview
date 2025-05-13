8e92c17dcdddbeead2ec7a777e61287cdced8d9a
Axel Blackburn <axelsoot@gmail.com>
2025-05-13 10:02:35 +0200

    1.2 is permutation

--- leet/src/lib.rs
+++ leet/src/lib.rs
@@ -1,6 +1,10 @@
 
 use std::collections::HashMap;
 
+
+// Problem 1.1: Is Unique
+// Implement an algorithm to determine if a string has all unique characters.
+
 // O(n)
 pub fn is_unique_1_1_hashmap(string: String) -> bool {
     let mut map = HashMap::new();
@@ -42,6 +46,54 @@ pub fn is_unique_1_1_no_external_data_structure(string: String) -> bool {
     true
 }
 
+// Problem 1.2: Check Permutation
+// Given two strings, write a method to decide if one is a permutation of the other.
+
+// O(n log n)
+pub fn is_permutation_1_2(string_a: String, string_b: String) -> bool {
+    if string_a.len() != string_b.len() {
+        return false;
+    }
+
+    let mut chars_a: Vec<_> = string_a.chars().collect();
+    chars_a.sort_unstable();
+    let mut chars_b: Vec<_> = string_b.chars().collect();
+    chars_b.sort_unstable();
+    for i in 0..chars_a.len() {
+        if chars_a[i] != chars_b[i] {
+            return false;
+        }
+    }
+    true
+}
+
+// O(n)
+pub fn is_permutation_1_2_hashmap(string_a: String, string_b: String) -> bool {
+    if string_a.len() != string_b.len() {
+        return false;
+    }
+
+    let mut map: HashMap<char, usize> = HashMap::new();
+    for c in string_a.chars() {
+        if map.contains_key(&c) {
+            *map.get_mut(&c).unwrap() += 1;
+        } else {
+            map.insert(c, 1);
+        }
+    }
+
+    for c in string_b.chars() {
+        if let Some(count) = map.get_mut(&c) {
+            *count -= 1;
+            if *count == 0 {
+                map.remove(&c);
+            }
+        } else {
+            return false;
+        }
+    }
+    true
+}
 
 #[cfg(test)]
 mod tests {
@@ -74,6 +126,38 @@ mod tests {
         assert_eq!(result, true);
         let test_string = String::from("abcdeafg");
         let result = is_unique_1_1_no_external_data_structure(test_string);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_is_permutation_1_2() {
+        let test_string_a = String::from("abcde");
+        let test_string_b = String::from("edcba");
+        let result = is_permutation_1_2(test_string_a, test_string_b);
+        assert_eq!(result, true);
+        let test_string_a = String::from("abcde");
+        let test_string_b = String::from("edcbaa");
+        let result = is_permutation_1_2(test_string_a, test_string_b);
+        assert_eq!(result, false);
+        let test_string_a = String::from("abcde");
+        let test_string_b = String::from("edcbe");
+        let result = is_permutation_1_2(test_string_a, test_string_b);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_is_permutation_1_2_hashmap() {
+        let test_string_a = String::from("abcde");
+        let test_string_b = String::from("edcba");
+        let result = is_permutation_1_2_hashmap(test_string_a, test_string_b);
+        assert_eq!(result, true);
+        let test_string_a = String::from("abcde");
+        let test_string_b = String::from("edcbaa");
+        let result = is_permutation_1_2_hashmap(test_string_a, test_string_b);
+        assert_eq!(result, false);
+        let test_string_a = String::from("abcde");
+        let test_string_b = String::from("edcbe");
+        let result = is_permutation_1_2_hashmap(test_string_a, test_string_b);
         assert_eq!(result, false);
     }
 }
